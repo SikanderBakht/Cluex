40d2485dcdb41e64239af17081bbbe09461ce339
SikanderBakht <sikander.bin.ismail@gmail.com>
2017-08-08 23:27:28 +0500

    HomeAlertRecyclerView Front End

--- app/src/main/java/com/example/cluex/Helper/HomeAlertActivityRecyclerViewHolder.java
+++ app/src/main/java/com/example/cluex/Helper/HomeAlertActivityRecyclerViewHolder.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,17 +20,21 @@ public class HomeAlertActivityRecyclerViewHolder extends RecyclerView.ViewHolder
 
     public TextView countryName;
     public ImageView countryPhoto;
+    public Button detailAlertButtonJava;
+    public Button immediateAlertButtonJava;
 
     public HomeAlertActivityRecyclerViewHolder(View itemView) {
         super(itemView);
         itemView.setOnClickListener(this);
         countryName = (TextView)itemView.findViewById(R.id.country_name);
         countryPhoto = (ImageView)itemView.findViewById(R.id.country_photo);
+        detailAlertButtonJava = (Button) itemView.findViewById(R.id.detail_alert_btn_xml);
+        immediateAlertButtonJava = (Button) itemView.findViewById(R.id.immediate_alert_btn_xml);
     }
 
     @Override
     public void onClick(View view) {
-      //  Toast.makeText(view.getContext(), "Clicked Country Position = " + getPosition(), Toast.LENGTH_SHORT).show();
+      //Toast.makeText(view.getContext(), "Clicked Country Position = " + getPosition(), Toast.LENGTH_SHORT).show();
 
 
     }
